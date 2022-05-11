32292d352a0404cc79388cb9c8baff3401677cab
Victorilies <103214820+Victorilies@users.noreply.github.com>
2022-05-11 21:11:38 +0300

    Victor
    Ilies

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,22 @@
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import Layout_content from "./components/Layout_content"
-import {StudenOverview} from "./components/StudenOverview"
+import { HeaderLayout } from './components/HeaderLayout';
+import {ContentLayout} from './components/Content_layout'
+import { Layout } from 'antd';
+import { card$ } from './mst/stores/Cardbox.store';
 
-
-const homeStudent = [
-  {
-    firstName: "Marin",
-    secondName:"Pesterean",
-    gender:"male",
-    age:21,
-    univerity: "UTM",
-    speciality:"CR",
-  },
-  {
-    firstName: "Marin",
-    secondName:"Pesterean",
-    gender:"male",
-    age:21,
-    univerity: "UTM",
-    speciality:"CR",
-  }
-
-]
 
 
 function App() {
+ 
 
-    const [cards, setCards] = useState([]);
-    const [student, setStudent] = useState([]);
-    
   return (
     <div className="App">
-        <Layout_content
-         setCards={setCards}
-        cards = {cards}
-        />
-
-        <StudenOverview 
-       
-        students = {homeStudent}
-        
-        />
-
-
-
-
+      <Layout className='layout'>
+      <HeaderLayout/>
+      <ContentLayout/>
+      </Layout>
     </div>
   );
 }
